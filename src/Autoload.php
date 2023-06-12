b28676715f8f09d429693e1268ab4c59451b7cf5
Nuno Maduro <enunomaduro@gmail.com>
2023-06-12 09:04:33 +0200

    Fixes global helpers from mockery

--- src/Autoload.php
+++ src/Autoload.php
@@ -3,6 +3,7 @@
 declare(strict_types=1);
 
 use Pest\Mock\Mock;
+use Pest\Mock\Mockery\Container;
 
 if (! function_exists('mock')) {
     /**
@@ -13,8 +14,12 @@ if (! function_exists('mock')) {
      * @param  class-string<TObject>|TObject  $object
      * @return Mock<TObject>
      */
-    function mock(string|object $object): Mock
+    function mock(object|string $object): Mock
     {
         return new Mock($object);
     }
+} else {
+    $reflection = new ReflectionClass(Mockery::class);
+
+    @file_put_contents(dirname($reflection->getFilename()) . '/helpers.php', '');
 }

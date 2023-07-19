1e5fe37b8aba4949ab6ee0f2d84db52456fd5e41
Nathanael Esayeas <nathanael.esayeas@protonmail.com>
2023-07-19 14:55:29 -0500

    Move `mock` function into `Pest\Mock` namespace
    Allows the use of `Pest\Mock\mock()` without overwriting Mockery's global helpers
    
    Signed-off-by: Nathanael Esayeas <nathanael.esayeas@protonmail.com>

--- src/Autoload.php
+++ src/Autoload.php
@@ -2,19 +2,17 @@
 
 declare(strict_types=1);
 
-use Pest\Mock\Mock;
+namespace Pest\Mock;
 
-if (! function_exists('mock')) {
-    /**
-     * Creates a new mock with the given class or object.
-     *
-     * @template TObject as object
-     *
-     * @param  class-string<TObject>|TObject  $object
-     * @return Mock<TObject>
-     */
-    function mock(object|string $object): Mock
-    {
-        return new Mock($object);
-    }
+/**
+ * Creates a new mock with the given class or object.
+ *
+ * @template TObject as object
+ *
+ * @param  class-string<TObject>|TObject  $object
+ * @return Mock<TObject>
+ */
+function mock(object|string $object): Mock
+{
+    return new Mock($object);
 }
